e69cffc5a599405066d1e39d1b45a1ccc850e2f8
VictorColominaAnoll <victorcolominanoll@gmail.com>
2020-02-09 20:50:56 +0100

    Interaccion con arboles

---
+++ FarmLand/Assets/Scripts/OnCollissionTree.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollission : MonoBehaviour {
+
+    public GameObject currentObject = null;
+
+    void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.CompareTag ("Tree")) {
+            Debug.Log ("JJ");
+            currentObject = other.gameObject;
+        }
+        else
+            Debug.Log ("74");
+    }
+}
---
+++ FarmLand/Assets/Scripts/collider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider : MonoBehaviour {
+    public GameObject currentObject = null;
+
+    void Update () {
+        if (Input.GetButtonDown ("Interact") && currentObject) {
+            currentObject.SendMessage("DoInteraction");
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.CompareTag ("Tree")) {
+            Debug.Log ("JJ");
+            currentObject = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other) {
+        if (other.CompareTag ("Tree")) {
+            if (other.gameObject == currentObject)
+                currentObject = null;
+        }
+    }
+}
